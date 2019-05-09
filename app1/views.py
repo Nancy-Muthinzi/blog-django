c7b06fa4dde8fe5676eca632858498970f2bd446
Nancy Muthinzi <nanciekathini@gmail.com>
2019-05-09 17:54:21 +0300

    make initial migration, create superuser, access admin page

--- app1/views.py
+++ app1/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+posts = [
+    {
+        'author': 'Nancy KM',
+        'title': 'Blog Post 1',
+        'content': 'Post Content 1',
+        'date_posted': '01/01/2019'
+    }
+]
 def home(request):
-    return render(request, 'app1/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'app1/home.html', context)
 
 def about(request):
     return render(request, 'app1/about.html')
